7df7f82055576f353cc9f266c1c506022aafce1d
Moadell <mo.adel@nu.edu.eg>
2020-06-30 16:04:41 +0200

    Updates

---
+++ precache-manifest.31940c62864f740426dcb6a1e74d43f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bda3dd4b61e48c02c3ba919b0217510",
+    "url": "/mohamedkhaled/index.html"
+  },
+  {
+    "revision": "dce32402797a04fd1411",
+    "url": "/mohamedkhaled/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5e836f5ef9f4c353336c",
+    "url": "/mohamedkhaled/static/js/2.d5ab471c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/mohamedkhaled/static/js/2.d5ab471c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce32402797a04fd1411",
+    "url": "/mohamedkhaled/static/js/main.c6598232.chunk.js"
+  },
+  {
+    "revision": "cb283fb59b2d4c56696f",
+    "url": "/mohamedkhaled/static/js/runtime-main.4bb7115f.js"
+  }
+]);
